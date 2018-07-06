b88d52293780f691817d14f4d0c85e6d017ff594
Hamza <hamza.ehsan@thedevden.co>
2018-07-06 16:39:52 +0500

    Test Comment Today

--- app/src/main/java/co/thedevden/firstapp/MainActivity.java
+++ app/src/main/java/co/thedevden/firstapp/MainActivity.java
@@ -1,15 +1,29 @@
 package co.thedevden.firstapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.myRecyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        String[] arr = {"Hello","C","C++","VB","Today","is","Friday","What","apple"};
+        recyclerView.setAdapter(new myAdapter(arr));
+
+
+
     }
+
 
     //Test Comment Check Now Quick
 }
